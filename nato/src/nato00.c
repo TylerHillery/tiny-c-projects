4ed3482f0a36027735f93dadedca4e6d859a446e
Tyler Hillery <tyhillery@gmail.com>
2026-06-19 12:51:50 -0400

    feat: nato v0

---
+++ nato/src/nato00.c
@@ -0,0 +1,66 @@
+/*
+ * Note: this code is not part of the book. When I started the chapter I like to
+ * try to think ahead of what the author might have the "tiny project" be. In
+ * this case I thought it would fun to take a string and convert each character
+ * to the NATO equivalent.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char *nato[] = {"Alfa",    "Bravo",  "Charlie", "Delta",    "Echo",
+                      "Foxtrox", "Golf",   "Hotel",   "India",    "Juliett",
+                      "Kilo",    "Lima",   "Mike",    "November", "Oscar",
+                      "Papa",    "Quebec", "Romeo",   "Sierra",   "Tango",
+                      "Uniform", "Victor", "Whiskey", "Xray",     "Yankee",
+                      "Whiskey", "Xray",   "Yankee",  "Zulu"};
+
+/*
+ * Returns the alpha index of the char if the char is a-z e.g. a is 0, b is 1...
+ * if the char is not alpha, return -1
+ */
+int get_alpha_index(char c) {
+  int i = (c & ~32) - 'A';
+  return i >= 0 && i < 26 ? i : -1;
+}
+
+/*
+ * Pass in a string and it will convert each character into the nato
+ * representation
+ * - case insensitive
+ * - ignores not alpabetic characters
+ * - returns array of char * pointers, NULL pointer indicates end of array
+ * - caller is responsible for freeing everything
+ */
+char **word_to_nato(char *s) {
+  size_t len = strlen(s);
+
+  // one extra for null ptr to end the array
+  char **result = (char **)malloc(sizeof(char *) * len + 1);
+  if (result == NULL) {
+    perror("malloc");
+    return NULL;
+  }
+
+  for (int i = 0; i < len; i++) {
+    // get the alpha index of the char
+    int alpha_index = get_alpha_index(s[i]);
+    if (alpha_index == -1) {
+      result[i] = strndup(&s[i], 1);
+    } else {
+      result[i] = strdup(nato[alpha_index]);
+    }
+  }
+  result[len] = NULL;
+  return result;
+}
+
+int main(void) {
+  char **words = word_to_nato("Hello, Tyler!");
+  for (int i = 0; words[i] != NULL; i++) {
+    if (i != 0 && strlen(words[i]) != 1)
+      printf(" ");
+    printf("%s", words[i]);
+  }
+  printf("\n");
+}
