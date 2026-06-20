b4bf1b4da931561d4e795a84bcfe430aa6b20565
Tyler Hillery <tyhillery@gmail.com>
2026-06-20 06:45:38 -0400

    refactor: optimize word parse

--- nato/src/word_parse01.c
+++ nato/src/word_parse01.c
@@ -9,21 +9,25 @@ char isterm(char *term) {
       "Oscar",  "Papa",    "Quebec",  "Romeo",  "Sierra", "Tango",   "Uniform",
       "Victor", "Whiskey", "Xray",    "Yankee", "Zulu"};
 
-  int x;
   const char *n, *t;
 
-  for (x = 0; x < (int)(sizeof(nato) / sizeof(nato[0])); x++) {
-    n = nato[x];
-    t = term;
-    while (*n != '\0') {
-      if ((*n | 0x20) != (*t | 0x20))
-        break;
-      n++;
-      t++;
-    }
-    if (*n == '\0') {
-      return *nato[x];
-    }
+  /*
+   * we can optimzie this, there is no need to loop through all nato words
+   * instead we can just compare the word with the letter it starts with
+   */
+  int alpha_index = (*term | 0x20) - 'a';
+
+  if (alpha_index < 0 || alpha_index > 25)
+    return '\0';
+
+  n = nato[alpha_index];
+  t = term;
+  // strcmp is not case insensitive so we compare char by char instead
+  while (*n != '\0') {
+    if ((*n | 0x20) != (*t | 0x20))
+      return '\0';
+    n++;
+    t++;
   }
-  return '\0';
+  return *nato[alpha_index];
 }
